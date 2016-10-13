bdbe96c620edb4308d52fb0f594091cce47c28ff
Raymond Ng <raymond.ng@visionsconnected.com>
2016-10-13 23:16:44 +0200

    Added application flow in form of comments

--- x-accounts-test/src/main/java/com/ximedes/App.java
+++ x-accounts-test/src/main/java/com/ximedes/App.java
@@ -2,4 +2,21 @@ package com.ximedes;
 
 public class App {
 
+	public static void main(String[] args) {
+
+		// spawn 10 thread to run the below
+
+			// while (amount times run = below 30)
+				// create merchant account
+
+				// create 1000 consumer accounts
+
+				// create 10 threads
+					// transfer 10 cents from bank account to 100 consumers
+					/* All 1000 cunsumers now has 10 cents */
+					// in random order 12 transfers are made from each consumer to ther merchant
+				// join the 10 threads
+				/* merchant should have 10000 cents */
+		// join the thread. This testing node is done
+	}
 }
