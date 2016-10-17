9630b8e62199a340dbe5da690261223fb5bbe3f2
Raymond Ng <raymond.ng@visionsconnected.com>
2016-10-17 22:03:43 +0200

    Added the ClientMaster so that it can create the bank account and start the slaves. The ClientMaster also measures the duration of the test.

---
+++ x-accounts/src/main/java/com/ximedes/MainClientMaster.java
@@ -0,0 +1,51 @@
+package com.ximedes;
+
+import com.ximedes.client.ClientMaster;
+
+import static java.lang.System.out;
+import java.util.*;
+
+/**
+ * The client's master main class.
+ * This master creates the bank account and starts the slaves
+ * Always start all the slaves, AppNodes and dbNode before the master
+ *
+ * @author Kees Jan Koster &lt;kjkoster@kjkoster.org&gt;
+ */
+public class MainClientMaster {
+
+	/**
+	 * Start the HTTP server to serve the API requests for the challenge.
+	 *
+	 * @param args
+	 *             Ignored.
+	 *
+	 * @throws Exception
+	 *                   When there was an error starting the server.
+	 */
+	public static void main(final String[] args) throws Exception {
+		// Some system properties are useful to tweak the network stack. This is
+		// relevant for the HTTPUrlConnection class, but does not hurt for any
+		// of the other code.
+		//
+		// The only relevant tweaking I found make a different is
+		// http.maxConnections. That reduces the number of actual network
+		// connections being created and helps avoid spurious
+		// "java.net.SocketException: Invalid argument" messages.
+		//
+		// Best start the JVM with:
+		// -Djava.net.preferIPv4Stack=true
+		// -Dhttp.keepalive=true
+		// -Dhttp.maxConnections=110
+		final Map<Object, Object> properties = System.getProperties();
+		for (final Map.Entry<Object, Object> entry : properties.entrySet()) {
+			final String key = (String)entry.getKey();
+			if (key.startsWith("http.") || key.startsWith("java.net.")) {
+				out.println(entry.getKey() + "=" + entry.getValue());
+			}
+		}
+
+		final ClientMaster clientMaster = new ClientMaster();
+		clientMaster.fireXimedesTest();
+	}
+}
---
+++ x-accounts/src/main/java/com/ximedes/client/ClientMaster.java
@@ -0,0 +1,97 @@
+package com.ximedes.client;
+
+import com.ximedes.API;
+import com.ximedes.http.HttpUrlConnectionApiClient;
+
+import java.io.*;
+import static java.lang.System.out;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ClientMaster {
+
+	private static final boolean trace = false;
+	private static final int amountTestingNodes = 1;
+
+	private final API api = new HttpUrlConnectionApiClient();
+	private final InetAddress multicastAddressStartCommand;
+	private final InetAddress multicastAddressListen;
+	private final int multicastPort = 24625;
+
+	public ClientMaster() throws UnknownHostException {
+		this.multicastAddressStartCommand = InetAddress.getByName("232.57.108.73");
+		this.multicastAddressListen = InetAddress.getByName("232.57.108.74");
+	}
+
+	public void fireXimedesTest() {
+		// log the start time
+		final LocalDateTime start = LocalDateTime.now();
+		out.println("== Test started at " + start.toString() + " ==");
+
+		// create the bank account
+		final int backAccountId = api.createAccount(30000000);
+
+		// send start command with the bankId
+		sendStartMessageToSlaves(backAccountId);
+
+		// wait for all nodes to be finished
+		listenForSlaves();
+
+		final LocalDateTime end = LocalDateTime.now();
+		out.println("== Test ended at " + end.toString() + " ==");
+		out.println("== Test duration " + Duration.between(start, end).getSeconds() + " seconds ==");
+	}
+
+	private void sendStartMessageToSlaves(int backAccountId) {
+		byte[] msg = ByteBuffer.allocate(4).putInt(backAccountId).array();
+
+		// Open a new DatagramSocket, which will be used to send the data.
+		try (DatagramSocket serverSocket = new DatagramSocket()) {
+			// Create a packet that will contain the data
+			// (in the form of bytes) and send it.
+			DatagramPacket msgPacket = new DatagramPacket(msg,
+														  msg.length,
+														  multicastAddressStartCommand,
+														  multicastPort);
+			if (trace) {
+				out.println("ClientMaster sends start message to ClienSlaves on multicast address " + multicastAddressStartCommand.getHostAddress() + ":" + multicastPort);
+			}
+
+			serverSocket.send(msgPacket);
+			serverSocket.close();
+		} catch (IOException ex) {
+			// what should be done when this is catched?
+			// for now output it
+			out.println("ERROR: " + ex.getMessage());
+		}
+	}
+
+	private void listenForSlaves() {
+		byte[] buf = new byte[256];
+
+		try (MulticastSocket clientSocket = new MulticastSocket(multicastPort)) {
+			clientSocket.joinGroup(multicastAddressListen);
+
+			if (trace) {
+				out.println("ClientMaster started to listen for slaves on " + multicastAddressListen.getHostAddress() + ":" + multicastPort);
+			}
+			for (int i = 0; i < amountTestingNodes; i++) {
+				DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
+				// clientSocket.receive is blocking
+				clientSocket.receive(msgPacket);
+
+				if (trace) {
+					String msg = new String(buf, 0, buf.length);
+					out.println("ClientSlave is done: " + msg);
+				}
+			}
+
+			clientSocket.leaveGroup(multicastAddressListen);
+			clientSocket.close();
+		} catch (IOException ex) {
+			out.println("ERROR: " + ex.getMessage());
+		}
+	}
+}
