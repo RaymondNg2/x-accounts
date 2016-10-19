2fb7d700114b4832777d410dd7d5d7934d91684c
Kees Jan Koster <kjkoster@kjkoster.org>
2016-10-19 22:47:00 +0200

    Cosmetics.

--- x-accounts/src/main/java/com/ximedes/Transaction.java
+++ x-accounts/src/main/java/com/ximedes/Transaction.java
@@ -1,17 +1,11 @@
 package com.ximedes;
 
-import static java.lang.System.currentTimeMillis;
-import static java.lang.System.out;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
 /**
  * The transaction DTO.
  * 
  * @author Kees Jan Koster &lt;kjkoster@kjkoster.org&gt;
  */
 public class Transaction {
-    private static final long start = currentTimeMillis();
-
     public Transaction(final int transactionId, final int from, final int to,
             final int amount, final Status status) {
         super();
@@ -21,17 +15,6 @@ public class Transaction {
         this.to = to;
         this.amount = amount;
         this.status = status;
-
-//        final long elapsedSeconds = MILLISECONDS.toSeconds(currentTimeMillis()
-//                - start);
-//        final long transactionsPerSecond = elapsedSeconds == 0L ? 0L
-//                : (transactionId / elapsedSeconds);
-//
-//        if ((transactionId % 100000) == 0) {
-//            out.println("TX[" + transactionId + "] " + amount + ": " + from
-//                    + " -> " + to + ".");
-//            out.println(transactionsPerSecond + " transactions per second.");
-//        }
     }
 
     public final int transactionId;
