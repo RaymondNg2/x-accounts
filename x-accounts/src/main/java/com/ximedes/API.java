818acb8f4690dae45ef3903da72a4e998459dfdd
Kees Jan Koster <kjkoster@kjkoster.org>
2016-10-19 22:47:20 +0200

    Implemented ping, getTransfer and getAccount HTTP GET methods. Fixed test cases to use these new calls.

--- x-accounts/src/main/java/com/ximedes/API.java
+++ x-accounts/src/main/java/com/ximedes/API.java
@@ -6,7 +6,6 @@ package com.ximedes;
  * @author Kees Jan Koster &lt;kjkoster@kjkoster.org&gt;
  */
 public interface API {
-
     /**
      * Create an account with the specified overdraft.
      * 
@@ -17,15 +16,44 @@ public interface API {
     int createAccount(int overdraft);
 
     /**
+     * Retrieve an account that was created earlier.
+     * 
+     * @param accountId
+     *            The identifier of the account to look up.
+     * @return The account as identified by the identifier, or <code>null</code>
+     *         if no account could be found for the specified id.
+     */
+    Account getAccount(int accountId);
+
+    /**
      * @param from
      *            the account to transfer from.
      * @param to
      *            The account to transfer to.
      * @param amount
      *            The amount to transfer.
-     * @return The resulting transaction.
+     * @return The resulting transfer identifier.
      */
-    Transaction transfer(int from, int to, int amount);
+    int transfer(int from, int to, int amount);
+
+    /**
+     * Retrieve a transfer that was scheduled earlier.
+     * 
+     * @param transferId
+     *            The identifier of the transfer to look up.
+     * @return The transfer as identified by the identifier, or
+     *         <code>null</code> if no transfer could be found for that id.
+     */
+    Transaction getTransfer(int transferId);
+
+    // The methods below are not specified by the challange documentation, but
+    // we added them for convenience.
+
+    /**
+     * An empty method to measure response times. The server should do nothing
+     * but send a response ASAP.
+     */
+    void ping();
 
     /**
      * Count the sum of all cents in the system. This call runs unsynchronised,
