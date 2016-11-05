8887ab06304381ce8f6a646c1ebb59edb58f5292
Kees Jan Koster <kjkoster@kjkoster.org>
2016-11-05 22:29:50 +0100

    The load master also acts as a slave.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@
 ( cd x-accounts; mvn3 package || exit 1 )
 
 scp x-accounts/target/x-accounts-0.0.1-SNAPSHOT.jar run.db0.sh db0:
-scp x-accounts/target/x-accounts-0.0.1-SNAPSHOT.jar run.load-master.sh load-master0:
+scp x-accounts/target/x-accounts-0.0.1-SNAPSHOT.jar run.load-master.sh run.load-slave.sh load-master0:
 scp x-accounts/target/x-accounts-0.0.1-SNAPSHOT.jar run.load-slave.sh load-slave0:
 
