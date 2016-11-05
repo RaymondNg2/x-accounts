4d830de520042063b5643a46e6df63be47ca29bd
Kees Jan Koster <kjkoster@kjkoster.org>
2016-11-05 22:28:28 +0100

    Proper start scripts with the right commands.

---
+++ run.load-master.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java8 -Xmx512M -Djava.net.preferIPv4Stack=true -cp /home/ec2-user/x-accounts-0.0.1-SNAPSHOT.jar com.ximedes.client.MainClientMaster
--- run.load-master0.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java8 -Xmx512M -Djava.net.preferIPv4Stack=true -cp /home/ec2-user/x-accounts-0.0.1-SNAPSHOT.jar com.ximedes.client.ClientMain
